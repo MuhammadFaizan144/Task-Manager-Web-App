9e65b770faae75455f54a8bca454e42853e466a9
Fazo01 <fg7829098@gmail.com>
2025-09-18 09:54:51 -0700

    Succesfull port and Connect tailwind

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 
 const Login = () => {
-  return (
-    <div>
-      Login
-    </div>
-  )
+    return (
+        <div class="flex flex-col items-center p-7 rounded-2xl">
+            
+            <div class="flex">
+                <span class="text-2xl font-medium">Class Warfare</span>
+                <span class="font-medium text-red-500">The Anti-Patterns</span>
+                <span class="flex gap-2 font-medium tex">
+                    <span>No. 4</span>
+                    <span>·</span>
+                    <span>2025</span>
+                </span>
+            </div>
+        </div>
+    )
 }
 
 export default Login
