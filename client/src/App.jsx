8740dc52bdad2ddfbc4f7298ebe77d298017f8ce
Fazo01 <fg7829098@gmail.com>
2025-09-20 11:30:50 -0700

    Routes setup in App.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,19 @@
-
+import Dashboard from './pages/Dashboard'
 import Login from './pages/Login'
+import {Route,Routes,Navigate,Outlet,useLocation} from "react-router-dom"
 
 function App() {
 
   return (
-    <Login/>
+    <main className='w-full min-h-screen bg-[#f3f4f6]'>
+      <Routes>
+        <Route element={<Layout/>}>
+          <Route path='/' element={<Navigate to={'/dashboard'}/>}/>
+          <Route path='/dashboard' element={<Dashboard/>}/>
+          <Route/>
+        </Route>
+      </Routes>
+    </main>
   )
 }
 
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,19 +1,9 @@
 import React from 'react'
-
 const Login = () => {
     return (
-        <div class="flex flex-col items-center p-7 rounded-2xl">
+        <main>
             
-            <div class="flex">
-                <span class="text-2xl font-medium text-blue-400">Class Warfare</span>
-                <span class="font-medium text-red-500">The Anti-Patterns</span>
-                <span class="flex gap-2 font-medium tex">
-                    <span>No. 4</span>
-                    <span>·</span>
-                    <span>2025</span>
-                </span>
-            </div>
-        </div>
+        </main>
     )
 }
 
