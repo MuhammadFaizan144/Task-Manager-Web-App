80252ad243aa31c8aca033f010b7101249ff9751
Fazo01 <fg7829098@gmail.com>
2025-09-20 11:19:20 -0700

    Install react-hook-form

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -5,7 +5,7 @@ const Login = () => {
         <div class="flex flex-col items-center p-7 rounded-2xl">
             
             <div class="flex">
-                <span class="text-2xl font-medium">Class Warfare</span>
+                <span class="text-2xl font-medium text-blue-400">Class Warfare</span>
                 <span class="font-medium text-red-500">The Anti-Patterns</span>
                 <span class="flex gap-2 font-medium tex">
                     <span>No. 4</span>
