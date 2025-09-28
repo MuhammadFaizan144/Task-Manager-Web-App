5f9bc9e0724261db81541d236b5ba8cdb7c2a5fc
Fazo01 <fg7829098@gmail.com>
2025-09-28 09:03:09 -0700

    Add Textbox inputs in clientPractice

--- clientPractice/src/components/Textbox.jsx
+++ clientPractice/src/components/Textbox.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-
-const Textbox = () => {
+import clsx from 'clsx'
+const Textbox = (({ placeholder, type, label, name, className, register, error }, ref) => {
   return (
-    <div>Textbox</div>
+    <div className='w-full flex flex-col gap-1'>
+      {label && <label htmlFor={name} className='text-slate-800'>{label}</label>}
+      <div>
+        <input type={type} name={name} {...register} placeholder={placeholder} className={clsx("bg-transparent px-3 py-2.5 2xl:py-3 border border-gray-300 placeholder-gray-400 text-gray-900 out text-base focus:ring-2 ring-blue-300",className)} aria-invalid={error ? "true" : "false"} />
+      </div>
+      {error && <span className='text-xs text-[#f64949fe] mt-0.5'>{error}</span>}
+    </div>
   )
-}
+})
 
 export default Textbox
